3c150b0409cee665863bf96e012463ad592450d5
kunal chopde <kunalchopde98@gmail.com>
2026-05-02 09:26:18 +0530

    done

---
+++ setup.py
@@ -0,0 +1,26 @@
+from typing import List
+from setuptools import setup,find_packages
+
+HYPEN_E_DOT="-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace ('/n',"")for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements 
+
+
+setup(
+    name="paddy-dataset",
+    author="kunal",
+    version="0.0.1",
+    author_email="kunalchopde98@gmail.com",
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages(),
+
+)
