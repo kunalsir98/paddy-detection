fce243b30387e147a5af3b2776e3be3aedc6f573
kunal chopde <kunalchopde98@gmail.com>
2026-05-02 10:11:52 +0530

    dataingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,52 +1,45 @@
+import sys 
 import os 
-import sys
 from src.logger import logging 
-from src.exception import CustomException 
-from dataclasses import dataclass 
+from src.exception import CustomException
+import pandas as pd
 from sklearn.model_selection import train_test_split 
-import pandas as pd 
+from dataclasses import dataclass 
 
-# Initalize the data Ingestion Configuration 
+#creating the data Ingestion Configuration
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts','train.csv')
+    train_data_path:str=os.path.join('artifacts','train.csv')
     test_data_path:str=os.path.join('artifacts','test.csv')
     raw_data_path:str=os.path.join('artifacts','raw.csv')
 
-# data ingestion
-class DataIngestion: 
+#creating the data Ingestion Class 
+class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
-
+    
     def initiate_data_ingestion(self):
-        logging.info('Data Ingestion Started')
         try:
             df=pd.read_csv('notebook/data.csv')
-            logging.info('Dataset read as pandas DataFrame')
-
+            logging.info('DataFrame Readen as pandas DataFrame ')
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False)
-
             logging.info('Train Test Split Initiated')
-
-            train_set,test_set=train_test_split(df,test_size=0.30,random_state=42)
-
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            logging.info('Train Test Split Completed')
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-
-            logging.info('Data Ingestion is Completed')
+            logging.info('Ingestion of the data is completed Succesfully')
 
             return(
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
+                self.ingestion_config.test_data_path
             )
-
+            
         except Exception as e:
-            logging.info('Error Occured in Data Ingestion Stage')
+            logging.info('Exception occured at data Ingestion Stage')
             raise CustomException(e,sys)
 
-    # Run Data Ingestion 
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-                
