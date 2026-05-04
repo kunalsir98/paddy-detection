49062cf6cf8c3ff56e152e8bbb50e4098c9baddf
kunal chopde <kunalchopde98@gmail.com>
2026-05-04 17:36:26 +0530

    done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,31 +1,28 @@
+import sys 
 import os 
-import sys  
 from src.logger import logging 
 from src.exception import CustomException
-from dataclasses import dataclass 
-from sklearn.preprocessing import StandardScaler,OneHotEncoder 
+from src.utils import save_object 
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
 from sklearn.compose import ColumnTransformer 
-from sklearn.pipeline import Pipeline 
-from sklearn.impute import SimpleImputer 
-from dataclasses import dataclass 
+from dataclasses import dataclass
+import numpy as np 
 import pandas as pd 
-import numpy as np
+from sklearn.impute import SimpleImputer
 from src.utils import save_object
-
-@dataclass
+@dataclass 
 class DataTransformationConfig:
-    prerocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
 
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
-    def get_data_transformation_obj(self):
+    def get_data_transformation_object(self):
         try:
-            logging.info('Data Transformation Started')
-            categorical_cols=['Agriblock', 'Variety', 'Soil Types', 'Nursery',
-       'Wind Direction_D1_D30', 'Wind Direction_D31_D60',
-       'Wind Direction_D61_D90', 'Wind Direction_D91_D120']
+            logging.info('Data Transformation initiated')
+
             numerical_cols=['Hectares ', 'Seedrate(in Kg)', 'LP_Mainfield(in Tonnes)',
        'Nursery area (Cents)', 'LP_nurseryarea(in Tonnes)', 'DAP_20days',
        'Weed28D_thiobencarb', 'Urea_40Days', 'Potassh_50Days',
@@ -40,14 +37,15 @@ class DataTransformation:
        'Inst Wind Speed_D91_D120(in Knots)', 'Relative Humidity_D1_D30',
        'Relative Humidity_D31_D60', 'Relative Humidity_D61_D90',
        'Relative Humidity_D91_D120', 'Trash(in bundles)']
+            categorical_cols=['Agriblock', 'Variety', 'Soil Types', 'Nursery',
+       'Wind Direction_D1_D30', 'Wind Direction_D31_D60',
+       'Wind Direction_D61_D90', 'Wind Direction_D91_D120']
             
-            logging.info('Pipeline Initiated')
-            
+            #creting preproceessor pipeline 
             num_pipeline=Pipeline(
                 steps=[
                     ('imputer',SimpleImputer(strategy='median')),
                     ('scaler',StandardScaler())
-
                 ]
             )
             cat_pipeline=Pipeline(
@@ -55,7 +53,6 @@ class DataTransformation:
                     ('imputer',SimpleImputer(strategy='most_frequent')),
                     ('one_hot_encoder',OneHotEncoder(handle_unknown='ignore')),
                     ('scaler',StandardScaler(with_mean=False))
-
                 ]
             )
             preprocessor=ColumnTransformer([
@@ -63,58 +60,59 @@ class DataTransformation:
                 ('cat_pipeline',cat_pipeline,categorical_cols)
             ])
 
-            return preprocessor 
+            logging.info('pipeline completed')
 
-            logging.info('pipeline Completed')
+            return preprocessor
+          
 
         except Exception as e:
-            logging.info('Exception Occured at Data Transformaation Stage')
+            logging.info('Exception occured in Data Transformation Stage')
             raise CustomException(e,sys)
-    
+        
     def initiate_data_transformation(self,train_path,test_path):
-        try:
-            #reading the train and test data 
-            train_df=pd.read_csv(train_path)
-            test_df=pd.read_csv(test_path)
+         try:
+             logging.info('Reading Train and Test Data')
+             train_df=pd.read_csv(train_path)
+             test_df=pd.read_csv(test_path)
 
-            logging.info('Read train and test data completed')
-            logging.info(f'Train DataFrame Head: \n{train_df.head().to_string()}')
-            logging.info(f'Test DataFrame Head: \n{test_df.head().to_string()}')
+             logging.info(f'getting columns for train and test data')
+             logging.info(f'Train Dataframe Head: \n :{train_df.head().to_string()}')
+             logging.info(f'Test Dataframe Head: \n :{test_df.head().to_string()}')
 
-            logging.info('Obtaining preprocessing object')
+             target_column_name='Paddy yield(in Kg)'
+             drop_columns=[target_column_name]
 
-            target_column_name='Paddy yield(in Kg)'
-            drop_columns=[target_column_name]
+             input_feature_train_df=train_df.drop(columns=drop_columns)
+             target_feature_train_df=train_df[target_column_name]
 
-            input_feature_train_df=train_df.drop(columns=drop_columns)
-            target_feature_train_df=train_df[target_column_name]
+             input_feature_test_df=test_df.drop(columns=drop_columns)
+             target_feature_test_df=test_df[target_column_name]
 
-            input_feature_test_df=test_df.drop(columns=drop_columns)
-            target_feature_test_df=test_df[target_column_name]
+             logging.info('Obtainig preprocessing Object')
 
-            preprocessing_obj=self.get_data_transformation_obj()
+             preprocessing_obj=self.get_data_transformation_object()
 
-            #transforming using preprocessor obj
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
-            logging.info('Applying preprocessing object on training and testing datasets ')
+             logging.info('Applying preprocessor object on train arr and test arr')
 
-            train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
-            test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+             train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
+             test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
 
-            save_object(self.data_transformation_config.prerocessor_obj_file_path,
-                        obj=preprocessing_obj
-                        )
-            
-            logging.info('Preprocessor pickle file saved')
+             save_object(
+                 self.data_transformation_config.preprocessor_obj_file_path,obj=preprocessing_obj
+             )
 
-            return(
-                train_arr,
-                test_arr,
-                self.data_transformation_config.prerocessor_obj_file_path
-            )
+             logging.info('Saving preprocessor file')
 
-        except Exception as e:
-            logging.info('Exception occured at initiate data transformation stage')
-            raise CustomException(e,sys)
+
+             return(
+                 train_arr,
+                 test_arr,
+                 self.data_transformation_config.preprocessor_obj_file_path 
+             )
+
+         except Exception as e:
+             logging.info('Exception Occured at Initiate Data Transformtion')
+             raise CustomException(e,sys)
---
+++ src/components/model_trainer.py
@@ -0,0 +1,81 @@
+import os 
+import sys 
+from src.exception import CustomException
+from src.logger import logging
+from sklearn.linear_model import LinearRegression 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression,LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor,AdaBoostRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from xgboost import XGBRegressor
+from catboost import CatBoostRegressor 
+from sklearn.svm import SVR
+from dataclasses import dataclass
+from src.utils import save_object
+from src.utils import evaluate_models,save_object
+
+
+@dataclass
+class ModelTrainerConfig:
+    model_trainer_file_path=os.path.join('artifacts','model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def inititate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("Splitting data into train and test spllit")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+
+            model={
+                'RandomForestRegressor':RandomForestRegressor(),
+                'DecissionTreeRegressor':DecisionTreeRegressor(),
+                "GradientBoodst":GradientBoostingRegressor(),
+                "LinearRegression":LinearRegression(),
+                "KneighbourRegressor":KNeighborsRegressor(),
+                "XGBRegressor":XGBRegressor(),
+                "CatboostRegressor":CatBoostRegressor(),
+                "SVM":SVR(),
+                "AdaBoostRegressor":AdaBoostRegressor(),
+
+            }
+
+            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,model)
+            print(model_report)
+            print('==========================')
+            logging.info("Model Report:{model_report}")
+
+
+            best_model_score=max(sorted(model_report.values()))
+
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model=model[best_model_name]
+
+
+            print(f"Best Model Selection,Model Name:{best_model_name} Model Score:{best_model_score}")
+            print("\n===========================================")
+            logging.info(f"Best Model Selection,Model Name:{best_model_name} Model Score:{best_model_score}")
+            logging.info("Hyperparameter tuning will Start after this")
+
+
+            save_object(
+                self.model_trainer_config.model_trainer_file_path,
+                obj=best_model
+            )
+
+        except Exception as e:
+            logging.info('Exception Occured at Model Trainer Stage')
+            raise CustomException(e,sys)
